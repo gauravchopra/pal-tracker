0f04d6fc5aeaa40f7e6c5ad124dabca39a08841f
gauravchopra <gaurav.chopra@rbs.com>
2019-08-26 16:57:19 +0530

    Test

---
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalTrackerApplicaiton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplicaiton.class,args);
+    }
+}
