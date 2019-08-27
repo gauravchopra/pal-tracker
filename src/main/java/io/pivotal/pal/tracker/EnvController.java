a5b1ba5583374a359a76e6e910a64a15f9b0599b
gauravchopra <gaurav.chopra@rbs.com>
2019-08-27 09:41:24 +0530

    New Changes

--- src/main/java/io/pivotal/pal/tracker/EnvController.java
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -1,4 +1,39 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class EnvController {
+
+    private String port;
+
+    private String memoryLimit;
+    private String cfInstanceIndex;
+    private String cfInstanceaddr;
+
+    public EnvController(@Value("${env.port:NOT SET}") String port,@Value("${memory.limit:NOT SET}") String memoryLimit,
+                         @Value("${cf.instance.index:NOT SET}")String cfInstanceIndex,
+                         @Value("${cf.instance.index:NOT SET}") String cfInstanceaddr) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceaddr = cfInstanceaddr;
+        this.cfInstanceIndex = cfInstanceIndex;
+    }
+
+    @RequestMapping("/env")
+    public Map<String,String> getEnv() {
+        Map<String,String> paramaters= new HashMap<>();
+        paramaters.put("PORT",port);
+        paramaters.put("MEMORY_LIMIT",memoryLimit);
+        paramaters.put("CF_INSTANCE_INDEX",cfInstanceIndex);
+        paramaters.put("CF_INSTANCE_ADDR",cfInstanceaddr);
+        return paramaters;
+    }
+
+
 }
--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PalTrackerApplicaiton {
+public class PalTrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PalTrackerApplicaiton.class,args);
+        SpringApplication.run(PalTrackerApplication.class,args);
     }
 }
